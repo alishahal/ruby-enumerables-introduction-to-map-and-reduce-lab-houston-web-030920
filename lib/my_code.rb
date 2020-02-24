120fef5b4f4a1b2e1ee02c4d05c4cffeec518b09
Karim Macknojia <alishahal@msn.com>
2020-02-24 19:36:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,55 @@
 # My Code here....
+def map_to_negativize(source_array)
+   final_array = []
+   counter = 0
+  while (counter<source_array.length) do
+    final_array<<source_array[counter]*(-1)
+    counter+=1
+  end
+   final_array
+end
+
+def map_to_no_change(source_array)
+   final_array =[]
+   counter=0
+  while (counter<source_array.length) do
+   final_array<<source_array[counter]*1
+   counter+=1
+  end
+   final_array
+end
+
+
+def map_to_double(source_array)
+  final_array =[]
+   counter=0
+  while (counter<source_array.length) do
+   final_array<<source_array[counter]*2
+   counter+=1
+  end
+   final_array
+end
+
+def map_to_square(source_array)
+  final_array =[]
+   counter=0
+  while (counter<source_array.length) do
+   final_array<<(source_array[counter])**2
+   counter+=1
+  end
+   final_array
+end
+
+def reduce_to_total(source_array, starting_point)
+   total=0
+   counter=0
+   
+end 
+
+def reduce_to_all_true(source_array)
+
+end
+
+def reduce_to_any_true(source_array)
+
+end
