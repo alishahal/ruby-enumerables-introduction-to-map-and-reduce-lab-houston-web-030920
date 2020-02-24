51f2e9a6c980cda3d33bbcd71c0ede254d1fbf34
Karim Macknojia <alishahal@msn.com>
2020-02-24 19:41:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,11 @@ end
 def reduce_to_total(source_array, starting_point=0)
    total= starting_point
    counter=0
-   
+   while (counter<source_array.length) do
+   total += source_array[counter]
+   counter+=1
+  end
+   total
 end 
 
 def reduce_to_all_true(source_array)
